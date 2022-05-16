0e7898b7feb4e83b457dfce51464384f48eadd45
Anthony Frandsen <anthonyfrandsen@gmail.com>
2022-05-15 20:21:00 -0400

    local time and page count

--- src/main/java/com/anthonyfrandsen/portfolio/PortfolioController.java
+++ src/main/java/com/anthonyfrandsen/portfolio/PortfolioController.java
@@ -5,13 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+
 @Controller
 public class PortfolioController {
 
-    @GetMapping("/index")
+    private int pageCount = 0;
+
+    @GetMapping("/")
     public String index(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
             Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("pageCount", ++pageCount);
+        model.addAttribute("time", LocalDateTime.now().toLocalTime());
         return "index";
     }
 }
